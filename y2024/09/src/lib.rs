2f273d0e2dd640e55cef21941690a2c55af76fbf
dcullina <dylanjcullinane@icloud.com>
2024-12-09 14:55:48 -0800

    modified day 9 part 1 solution

--- y2024/09/src/lib.rs
+++ y2024/09/src/lib.rs
@@ -1,49 +1,45 @@
 pub fn part_one(input: &str) -> usize {
+    let mut disk: Vec<usize> = input.chars()
+        .filter_map(|character| character.to_digit(10))
+        .map(|digit| digit as usize)
+        .collect();
+
+    let mut checksum: usize = 0;
+    let mut flat_index: usize = 0;
+
+    let mut front_index: usize = 0;
+    let mut back_index: usize = disk.len() - 1;
+
+    while front_index <= back_index {
+        if (front_index % 2) == 1 { // if we are in free space
+            // loop through the size of the free space
+            for _ in 0..disk[front_index] {
+                while disk[back_index] <= 0 {
+                    back_index -= 2;
+                }
+                let id: usize = back_index / 2;
+                disk[back_index] -= 1;
+                checksum += flat_index * id;
+                flat_index += 1;
+            }
+        } else {
+            let id: usize = front_index / 2;
+            for _ in 0..disk[front_index] {
+                checksum += flat_index * id;
+                flat_index += 1;
+            }
+        }
+        front_index += 1;
+    }
+    checksum
+}
+
+
+
+pub fn part_two(input: &str) -> usize {
     let block_code: Vec<u8> = input.chars()
         .filter_map(|character| character.to_digit(10))
         .map(|digit| digit as u8)
         .collect();
-    let id_code: Vec<i32> = convert_to_id_code(block_code);
-    calculate_checksum(id_code)
-}
-
-fn convert_to_id_code(block_code: Vec<u8>) -> Vec<i32> {
-    let mut result: Vec<i32> = Vec::new();
-    let mut id: u32 = 0;
-    for (index, &digit) in block_code.iter().enumerate() {
-        if (index % 2) == 0 {
-            for _ in 0..digit {
-                result.push(id as i32);
-            }
-            id += 1;
-        } else {
-            for _ in 0..digit {
-                result.push(-1);
-            }
-        }
-    }
-    result
-}
-
-fn calculate_checksum(mut id_code: Vec<i32>) -> usize {
-    let mut sum: usize = 0;
-    let mut index: usize = 0;
-    while !id_code.is_empty() {
-        if index >= id_code.len() { break; }
-        if id_code[id_code.len() - 1] == -1 { 
-            id_code.pop();
-            continue;
-        }
-        if id_code[index] == -1 {
-            sum += index * id_code.pop().unwrap_or(0) as usize;
-        } else {
-            sum += index * id_code[index] as usize;
-        }
-        index += 1;
-    }
-    sum
-}
-
-pub fn part_two(input: &str) -> usize {
     1
 }
--- y2024/09/tests/tests.rs
+++ y2024/09/tests/tests.rs
@@ -11,6 +11,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let test_input: &str = include_str!("../test_input.txt");
-        assert_eq!(part_two(test_input), 0);
+        assert_eq!(part_two(test_input), 2858);
     }
 }
