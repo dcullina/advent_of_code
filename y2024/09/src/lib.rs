02d89b211e5a0421ab966c0255542e16c41f87b9
dcullina <dylanjcullinane@icloud.com>
2024-12-09 19:32:32 -0800

    solved day 9 part 2

--- y2024/09/src/lib.rs
+++ y2024/09/src/lib.rs
@@ -37,9 +37,72 @@ pub fn part_one(input: &str) -> usize {
 
 
 pub fn part_two(input: &str) -> usize {
-    let block_code: Vec<u8> = input.chars()
+    let disk: Vec<usize> = input.chars()
         .filter_map(|character| character.to_digit(10))
-        .map(|digit| digit as u8)
+        .map(|digit| digit as usize)
         .collect();
-    1
+
+    // convert it to a vector of (file_id/EmptySpace, size)
+    let mut file_rep: Vec<(Option<usize>, usize, bool)> = disk
+        .iter()
+        .enumerate()
+        .map(|(index, &size)| {
+            if (index % 2) == 1 {
+                (None, size, false)
+            } else {
+                (Some(index / 2), size, false)
+            }
+        }).collect();
+
+    let mut back_index: usize = file_rep.len() - 1; // set at the last file
+
+    'outer: while back_index > 0 {
+        // if we visited this or if it's empty space skip it
+        if file_rep[back_index].2 || file_rep[back_index].0.is_none() {
+            back_index -= 1;
+            continue;
+        }
+
+        for index in 0..back_index {
+            match file_rep[index].0 {
+                None => {
+                    // if it fits in the blank space, insert it, remove it, then break
+                    if file_rep[index].1 >= file_rep[back_index].1 {
+                        // insert that element into the space
+                        file_rep.insert(index, (
+                            file_rep[back_index].0,
+                            file_rep[back_index].1,
+                            true,
+                        ));
+                        // subtract the size of the white space
+                        file_rep[index + 1].1 -= file_rep[index].1;
+
+                        // change that element to empty space
+                        file_rep[back_index + 1].0 = None;
+                        continue 'outer; // this will restart the outer loop with back index pointed at the next variable now
+                    }
+                },
+                _ => continue
+            }
+        }
+        // if there are no matches, bring the back index down by one
+        back_index -= 1;
+    }
+
+
+    let mut checksum: usize = 0; // used to collect id * index sum for each block
+    let mut flat_index: usize = 0; // used for tracking the index to calculate checksum
+
+    file_rep.iter().for_each(|element| {
+        if let Some(file_id) = element.0 {
+            for _ in 0..element.1 {
+                checksum += file_id * flat_index;
+                flat_index += 1;
+            }
+        } else {
+            flat_index += element.1;
+        }
+    });
+
+    checksum
 }
