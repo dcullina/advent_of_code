9ec9c2066cc4c82f0e8501d39f812e78e928c11f
dcullina <dylanjcullinane@icloud.com>
2024-12-11 19:16:14 -0800

    a queue doesn't help

--- y2024/11/src/lib.rs
+++ y2024/11/src/lib.rs
@@ -1,68 +1,51 @@
 use std::time::Instant;
-use rayon::prelude::*;
+use std::collections::VecDeque;
 
 const MUL_YEAR: usize = 2024;
 
 pub fn part_one_two(input: &str, n_blinks: usize) -> usize {
-    let mut stones: Vec<usize> = Vec::new();
-    input
+    let start = Instant::now();
+    let mut initial_stones: Vec<usize> = input
         .split_ascii_whitespace()
-        .for_each(|number| {
-            stones.push(number.parse::<usize>().unwrap());
-        });
+        .map(|number| {
+            number.parse::<usize>().unwrap()
+        })
+        .collect();
 
-    for i in 0..n_blinks {
-        let start = Instant::now();
-        let mut new_stones: Vec<usize> = Vec::new();
-        let mut index: usize = 0;
+    let mut total_stones: usize = 0;
+    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
 
-        while index < stones.len() {
-            if stones[index] == 0 {
-                stones[index] = 1;
-            } else {
-                let mut stone_value: usize = stones[index];
-                let mut stone_value_digits: Vec<usize> = Vec::new();
-
-                while stone_value > 0 {
-                    stone_value_digits.push(stone_value % 10);
-                    stone_value /= 10;
-                }
-                stone_value_digits.reverse();
-
-                if stone_value_digits.len() % 2 == 0 {
-                    let mid = stone_value_digits.len() / 2;
-                    let left = stone_value_digits[..mid].iter().fold(0, |acc, &d| acc * 10 + d);
-                    let right = stone_value_digits[mid..].iter().fold(0, |acc, &d| acc * 10 + d);
-
-                    stones[index] = left;
-                    new_stones.push(right);
-                } else {
-                    stones[index] *= MUL_YEAR;
-                }
-
-                /*if stone_value.chars().count() % 2 == 0 {
-
-                    let (left, right) = val_str.split_at(val_str.len() / 2);
-
-                    stones[index] = left.parse::<usize>().unwrap();
-
-                    if stones.len() == index + 1 {
-                        stones.push(right.parse::<usize>().unwrap());
-                    } else {
-                        stones.insert(index + 1, right.parse::<usize>().unwrap())
-                    }
-
-                    index += 1;
-                } else {
-                    stones[index] *= MUL_YEAR;
-                }
-                */
-            }
-            index += 1;
-        }
-        stones.append(&mut new_stones);
-        let duration = start.elapsed();
-        println!("Finished #{} in {:?}", i + 1, duration);
+    for stone in initial_stones.drain(..) {
+        queue.push_back((stone, n_blinks));
     }
-    stones.len()
+
+    while let Some((mut value, remaining_blinks)) = queue.pop_front() {
+        if remaining_blinks == 0 {
+            total_stones += 1;
+            continue;
+        }
+
+        if value == 0 {
+            queue.push_back((1, remaining_blinks - 1));
+        } else {
+            let digits = value.to_string();
+            let digits_count = digits.len();
+
+            if digits_count % 2 == 0 {
+                let mid = digits_count / 2;
+                let left = digits[..mid].parse::<usize>().unwrap();
+                let right = digits[mid..].parse::<usize>().unwrap();
+
+                queue.push_back((left, remaining_blinks - 1));
+                queue.push_back((right, remaining_blinks - 1));
+            } else {
+                value *= MUL_YEAR;
+                queue.push_back((value, remaining_blinks - 1));
+            }
+        }
+    }
+
+    let duration = start.elapsed();
+    println!("Finished Execution in: {:?}", duration);
+    total_stones
 }
