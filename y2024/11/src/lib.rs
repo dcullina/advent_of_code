47a3d4128f01f724a76c5e7cf02b3a74d06090e2
dcullina <dylanjcullinane@icloud.com>
2024-12-11 21:07:54 -0800

    solved day 11 part 2, hashmaps worked better

--- y2024/11/src/lib.rs
+++ y2024/11/src/lib.rs
@@ -1,51 +1,53 @@
-use std::time::Instant;
-use std::collections::VecDeque;
+use std::collections::HashMap;
 
 const MUL_YEAR: usize = 2024;
 
-pub fn part_one_two(input: &str, n_blinks: usize) -> usize {
-    let start = Instant::now();
-    let mut initial_stones: Vec<usize> = input
-        .split_ascii_whitespace()
-        .map(|number| {
-            number.parse::<usize>().unwrap()
-        })
-        .collect();
-
-    let mut total_stones: usize = 0;
-    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
-
-    for stone in initial_stones.drain(..) {
-        queue.push_back((stone, n_blinks));
-    }
-
-    while let Some((mut value, remaining_blinks)) = queue.pop_front() {
-        if remaining_blinks == 0 {
-            total_stones += 1;
-            continue;
-        }
-
-        if value == 0 {
-            queue.push_back((1, remaining_blinks - 1));
+fn update_stone_counts(stone_count_map: &HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut new_stone_count_map: HashMap<usize, usize> = HashMap::new();
+    
+    for (&stone_value, count) in stone_count_map {
+        if stone_value == 0 {
+            *new_stone_count_map.entry(1).or_insert(0) += count;
         } else {
-            let digits = value.to_string();
+            
+            let digits = stone_value.to_string();
             let digits_count = digits.len();
 
             if digits_count % 2 == 0 {
+
                 let mid = digits_count / 2;
                 let left = digits[..mid].parse::<usize>().unwrap();
                 let right = digits[mid..].parse::<usize>().unwrap();
 
-                queue.push_back((left, remaining_blinks - 1));
-                queue.push_back((right, remaining_blinks - 1));
+                *new_stone_count_map.entry(left).or_insert(0) += count;
+                *new_stone_count_map.entry(right).or_insert(0) += count;
+              
+
             } else {
-                value *= MUL_YEAR;
-                queue.push_back((value, remaining_blinks - 1));
+                *new_stone_count_map.entry(stone_value * MUL_YEAR).or_insert(0) += count;
             }
         }
     }
 
-    let duration = start.elapsed();
-    println!("Finished Execution in: {:?}", duration);
-    total_stones
+    new_stone_count_map
+}
+
+pub fn part_one_two(input: &str, n_blinks: usize) -> usize {
+
+    let mut stone_counts: HashMap<usize, usize> = HashMap::new();
+
+    input
+        .split_ascii_whitespace()
+        .for_each(|number| {
+            let stone_value: usize = number.parse::<usize>().unwrap();
+            *stone_counts.entry(stone_value).or_insert(0) += 1;
+        });
+
+    for _ in 0..n_blinks {
+        
+        stone_counts = update_stone_counts(&stone_counts);
+
+    }
+
+    stone_counts.values().sum()
 }
