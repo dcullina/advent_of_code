d19a43f00ae7ae4e6213d582dfdd9666c80739e9
dcullina <dylanjcullinane@icloud.com>
2024-12-13 08:45:07 -0800

    solved day 13 part 1

--- y2024/13/src/lib.rs
+++ y2024/13/src/lib.rs
@@ -1,5 +1,58 @@
+use core::num;
+
 pub fn part_one(input: &str) -> usize {
-    1
+    let machines: Vec<Vec<(usize,usize)>> = input
+        .split("\n\n")
+        .map(|machine| {
+            machine
+                .lines()
+                .enumerate()
+                .map(|(index, command)| {
+                    if index == 2 {
+                        let split_command: Vec<&str> = command.split(&['=', ','][..]).collect();
+                        (split_command[1].parse::<usize>().unwrap(), split_command[3].parse::<usize>().unwrap())
+                    } else {
+                        let split_command: Vec<&str> = command.split(&['+', ','][..]).collect();
+                        (split_command[1].parse::<usize>().unwrap(), split_command[3].parse::<usize>().unwrap())
+                    }
+                })
+                .collect()
+        })
+        .collect();
+
+    machines.iter()
+        .map(|machine| {
+            calculate_cost(machine)
+        })
+        .sum()
+}
+
+fn calculate_cost(machine: &Vec<(usize, usize)>) -> usize {
+    let A: (isize, isize) = (machine[0].0 as isize, machine[0].1 as isize);
+    let B: (isize, isize) = (machine[1].0 as isize, machine[1].1 as isize);
+    let P: (isize, isize) = (machine[2].0 as isize, machine[2].1 as isize);
+
+    let a_det: isize = P.0 * B.1 - P.1 * B.0;
+    let b_det: isize = A.0 * P.1 - A.1 * P.0;
+    let det: isize = A.0 * B.1 - A.1 * B.0;
+
+    let a: Option<isize> = check_division(a_det, det);
+    let b: Option<isize> = check_division(b_det, det);
+    if a == None || b == None {
+        return 0;
+    }
+
+    (a.unwrap() * 3 + b.unwrap()) as usize
+}
+
+fn check_division(numerator: isize, denominator: isize) -> Option<isize> {
+    if denominator == 0 {
+        None
+    } else if numerator % denominator != 0 {
+        None
+    } else {
+        Some( numerator / denominator )
+    }
 }
 
 pub fn part_two(input: &str) -> usize {
--- y2024/13/tests/tests.rs
+++ y2024/13/tests/tests.rs
@@ -6,7 +6,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let test_input: &str = include_str!("../test_input.txt");
-        assert_eq!(part_one(test_input), 0);
+        assert_eq!(part_one(test_input), 480);
     }
 
     #[test]
